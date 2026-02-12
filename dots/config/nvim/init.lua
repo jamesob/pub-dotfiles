134f06fbf78b1febb3ca948704829241b9c70d72
James O'Beirne <github@au92.org>
2026-02-11 22:43:12 -0500

    mac

--- dots/config/nvim/init.lua
+++ dots/config/nvim/init.lua
@@ -58,18 +58,6 @@ require("lazy").setup({
   {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    config = function()
-      require('nvim-treesitter.configs').setup({
-        ensure_installed = synlangs,
-        sync_install = false,
-        auto_install = true,
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-        },
-        indent = { enable = true },
-      })
-    end,
   },
   { 'folke/which-key.nvim' },
 
